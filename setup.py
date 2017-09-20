cb8c06454e05c864272d16daeffe2bf912be94d0
Ye Ding <guiewy@gmail.com>
2017-09-21 00:29:58 +0800

    v0.9.20

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='deepauth',
-    version='0.9.19',
+    version='0.9.20',
     packages=['deepauth', 'deepauth.utils'],
     url='http://open.deepera.com',
     license='CPL-3.0',
