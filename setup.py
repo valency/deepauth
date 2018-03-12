eb07fb59676cfde078f29b12866b151d270b8a02
Ye Ding <yding@deepera.com>
2018-03-12 15:40:21 +0800

    v1.3.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='deepauth',
-    version='0.9.20',
+    version='1.3.12',
     packages=['deepauth', 'deepauth.utils'],
     url='http://open.deepera.com',
     license='CPL-3.0',
